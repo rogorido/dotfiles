720035ed80b13caef9e61790830bafbbd9e925a3
Igor Sosa Mayor <joseleopoldo1792@gmail.com>
2025-08-18 07:09:05 +0200

    Delete embark config.

--- home/dot_emacs.d/emacs-personal/ism-consultvertico.el
+++ home/dot_emacs.d/emacs-personal/ism-consultvertico.el
@@ -189,41 +189,6 @@
   ;; Enable recursive minibuffers
   (setq enable-recursive-minibuffers t))
 
-;; (use-package embark
-;;   :ensure t
-
-;;   :bind
-;;   (("s-e" . embark-act)         ;; pick some comfortable binding
-;;    ("C-;" . embark-dwim)        ;; good alternative: M-.
-;;    ("C-h B" . embark-bindings)) ;; alternative for `describe-bindings'
-
-;;   :init
-
-;;   ;; Optionally replace the key help with a completing-read interface
-;;   (setq prefix-help-command #'embark-prefix-help-command)
-
-;;   ;; Show the Embark target at point via Eldoc. You may adjust the
-;;   ;; Eldoc strategy, if you want to see the documentation from
-;;   ;; multiple providers. Beware that using this can be a little
-;;   ;; jarring since the message shown in the minibuffer can be more
-;;   ;; than one line, causing the modeline to move up and down:
-
-;;   ;; (add-hook 'eldoc-documentation-functions #'embark-eldoc-first-target)
-;;   ;; (setq eldoc-documentation-strategy #'eldoc-documentation-compose-eagerly)
-
-;;   :config
-
-;;   ;; Hide the mode line of the Embark live/completions buffers
-;;   (add-to-list 'display-buffer-alist
-;;                '("\\`\\*Embark Collect \\(Live\\|Completions\\)\\*"
-;;                  nil
-;;                  (window-parameters (mode-line-format . none)))))
-
-;; ;; Consult users will also want the embark-consult package.
-;; (use-package embark-consult
-;;   :ensure t ; only need to install it, embark loads it after consult if found
-;;   :hook
-;;   (embark-collect-mode . consult-preview-at-point-mode))
 
 (use-package orderless
   :ensure t
@@ -231,53 +196,6 @@
   (completion-styles '(orderless basic))
   (completion-category-overrides '((file (styles basic partial-completion))
                                    (buffer (styles basic)))))
-
-;; (use-package corfu
-;;   ;; Optional customizations
-;;   :custom
-;;   ;; (corfu-cycle t)                ;; Enable cycling for `corfu-next/previous'
-;;   (corfu-auto t)                 ;; Enable auto completion
-;;   ;; (corfu-separator ?\s)          ;; Orderless field separator
-;;   ;; (corfu-quit-at-boundary nil)   ;; Never quit at completion boundary
-;;   ;; (corfu-quit-no-match nil)      ;; Never quit, even if there is no match
-;;   ;; (corfu-preview-current nil)    ;; Disable current candidate preview
-;;   ;; (corfu-preselect 'prompt)      ;; Preselect the prompt
-;;   ;; (corfu-on-exact-match nil)     ;; Configure handling of exact matches
-;;   ;; (corfu-scroll-margin 5)        ;; Use scroll margin
-
-;;   ;; Enable Corfu only for certain modes.
-;;   ;; :hook ((prog-mode . corfu-mode)
-;;   ;;        (shell-mode . corfu-mode)
-;;   ;;        (eshell-mode . corfu-mode))
-
-;;   ;; Recommended: Enable Corfu globally.  This is recommended since Dabbrev can
-;;   ;; be used globally (M-/).  See also the customization variable
-;;   ;; `global-corfu-modes' to exclude certain modes.
-;;   :init
-;;   (global-corfu-mode))
-
-;; ;; A few more useful configurations...
-;; (use-package emacs
-;;   :init
-;;   ;; TAB cycle if there are only few candidates
-;;   (setq completion-cycle-threshold 3)
-
-;;   ;; Emacs 28: Hide commands in M-x which do not apply to the current mode.
-;;   ;; Corfu commands are hidden, since they are not supposed to be used via M-x.
-;;   ;; (setq read-extended-command-predicate
-;;   ;;       #'command-completion-default-include-p)
-
-;;   ;; Enable indentation+completion using the TAB key.
-;;   ;; `completion-at-point' is often bound to M-TAB.
-;;   (setq tab-always-indent 'complete))
-
-;; no sé cómo funciona esto...
-;; (use-package consult-dir
-;;   :ensure t
-;;   :bind (("C-x C-d" . consult-dir)
-;;          :map minibuffer-local-completion-map
-;;          ("C-x C-d" . consult-dir)
-;;          ("C-x C-j" . consult-dir-jump-file)))
 
 
 (provide 'ism-consultvertico)
