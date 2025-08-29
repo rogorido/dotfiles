2743757ec9444c2287dbf35b8f782bea7f1ab2fc
Igor Sosa Mayor <joseleopoldo1792@gmail.com>
2025-08-29 15:59:11 +0200

    Miscellaneous for emacs.

--- home/dot_emacs.d/emacs-personal/ism-misc.el
+++ home/dot_emacs.d/emacs-personal/ism-misc.el
@@ -15,19 +15,78 @@
 (setq uniquify-buffer-name-style 'post-forward)
 
 ;; con esto se consigue que no salga una ventana para lo de la
-;; contraseña. 
+;; contraseña.
 (setq epg-pinentry-mode 'loopback)
 
 ;; Dar permisos de ejecución al fichero si es un script
 (setq after-save-hook
       (quote (executable-make-buffer-file-executable-if-script-p)))
 
-;; Cosas de man: 
+;; Cosas de man:
 ;(setq Man-notify-method 'pushy)
 ;; con esto se consigue que la anchura sea esta
 (setenv "MANWIDTH" "72")
 
 (setq olivetti-body-width 72)
+
+(use-package nov
+  :config
+  (setq nov-text-width 80)
+  ;;(setq nov-text-width t)
+  ;;(setq visual-fill-column-center-text t)
+  ;;(add-hook 'nov-mode-hook 'visual-line-mode)
+  ;;(add-hook 'nov-mode-hook 'visual-fill-column-mode)
+
+  (defun my/nov-font-setup ()
+    (face-remap-add-relative 'variable-pitch :family "Gentium Plus"
+                             :height 1.2))
+  (add-hook 'nov-mode-hook 'my/nov-font-setup)
+  (add-to-list 'auto-mode-alist '("\\.epub\\'" . nov-mode))
+  )
+
+;; Chezmoi for managing dotfiles
+;; https://www.chezmoi.io/
+;; https://github.com/tuh8888/chezmoi.el
+(add-to-list 'load-path "~/giteando/chezmoi.el")
+(add-to-list 'load-path "~/giteando/chezmoi.el/extensions")
+(use-package chezmoi)
+(require 'chezmoi-dired)
+(require 'chezmoi-magit)
+
+(add-to-list 'load-path "~/giteando/emacs-eat")
+(use-package eat
+  :config
+  (setq eat-kill-buffer-on-exit t))
+
+(use-package csv-mode
+  :ensure t
+         ;; Hook order matters:
+  :hook ((csv-mode . csv-align-mode)            ;; Show the CSV in a tabular format
+         ;; (csv-mode . csv-highlight)             ;; Add colours to columns for clearer reading
+         (csv-mode . csv-guess-set-separator))) ;; Guess how to parse the CSV automatically
+
+(use-package ssh-config-mode
+  :ensure t
+  :mode (("/\\.ssh/config\\(\\.d/.*\\.conf\\)?\\'" . ssh-config-mode)
+         ("/sshd?_config\\(\\.d/.*\\.conf\\)?\\'" . ssh-config-mode)
+         ("/known_hosts\\'"       . ssh-known-hosts-mode)
+         ("/authorized_keys2?\\'" . ssh-authorized-keys-mode))
+  :hook (((ssh-config-mode ssh-known-hosts-mode ssh-authorized-keys-mode) . turn-on-font-lock)))
+
+
+;; Other packages
+(add-to-list 'load-path "~/giteando/emacs-request/")
+(add-to-list 'load-path "~/giteando/anki-editor/")
+;(setq request-log-level 'debug)
+;(setq request-log-level 'error)
+
+;; esto parece que deeshabilita completamente lo de vc-mode
+;; (setq vc-handled-backends nil)
+
+;(require 'keychain-environment)
+(keychain-refresh-environment)
+(defalias 'cargar-keychain 'keychain-refresh-environment)
+
 
 (provide 'ism-misc)
 ;;; ism-misc.el ends here
