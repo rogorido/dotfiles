48f78526fe4de3bb8b82574138eb2d84a4c6fa4e
Igor Sosa Mayor <joseleopoldo1792@gmail.com>
2025-08-29 15:59:42 +0200

    Own config file for corfu, cape, etc.

---
+++ home/dot_emacs.d/emacs-personal/ism-corfucape.el
@@ -0,0 +1,71 @@
+(message "Loading corfu and cape settings...")
+
+(setq completion-ignore-case t)
+
+(use-package corfu
+  :ensure t
+  ;; Optional customizations
+  :custom
+  ;; (corfu-cycle t)                ;; Enable cycling for `corfu-next/previous'
+  (corfu-auto t) ;; Enable auto completion
+  (corfu-auto-prefix 2) ;; Enable auto completion
+  (corfu-auto-delay 0.15) ;; Enable auto completion
+  ;; (corfu-separator ?\s)          ;; Orderless field separator
+  ;; (corfu-quit-at-boundary nil)   ;; Never quit at completion boundary
+  (corfu-quit-no-match t)      ;; Never quit, even if there is no match
+  ;; (corfu-preview-current nil)    ;; Disable current candidate preview
+  (corfu-preselect 'prompt)      ;; Preselect the prompt
+  ;; (corfu-on-exact-match nil)     ;; Configure handling of exact matches
+  ;; (corfu-scroll-margin 5)        ;; Use scroll margin
+
+  ;; Enable Corfu only for certain modes.
+  ;; :hook ((prog-mode . corfu-mode)
+  ;;        (shell-mode . corfu-mode)
+  ;;        (eshell-mode . corfu-mode))
+
+  ;; Recommended: Enable Corfu globally.  This is recommended since Dabbrev can
+  ;; be used globally (M-/).  See also the customization variable
+  ;; `global-corfu-modes' to exclude certain modes.
+  :bind
+  ;; Configure SPC for separator insertion
+  (:map corfu-map ("SPC" . corfu-insert-separator))
+  :init
+  (global-corfu-mode))
+
+
+(use-package cape
+  :ensure t
+  ;; Bind prefix keymap providing all Cape commands under a mnemonic key.
+  ;; Press C-c p ? to for help.
+  :bind ("C-c p" . cape-prefix-map) ;; Alternative keys: M-p, M-+, ...
+  ;; Alternatively bind Cape commands individually.
+  ;; :bind (("C-c p d" . cape-dabbrev)
+  ;;        ("C-c p h" . cape-history)
+  ;;        ("C-c p f" . cape-file)
+  ;;        ...)
+
+  :init
+  ;; Add to the global default value of `completion-at-point-functions' which is
+  ;; used by `completion-at-point'.  The order of the functions matters, the
+  ;; first function returning a result wins.  Note that the list of buffer-local
+  ;; completion functions takes precedence over the global list.
+
+  ;; atención: me parece que es mejor poner el orden inverso porque realmente hace un push
+  ;; y por tanto casí me aparece lo del dict como primera opción.
+  (add-hook 'completion-at-point-functions #'cape-dabbrev)
+  (add-hook 'completion-at-point-functions #'cape-dict)
+
+  )
+
+(setq cape-dict-file "/home/igor/.dict"
+      cape-dict-limit 30)
+
+;; esto tal vez sea un poco agresivo, pero es que no encuentro la forma
+;; de decirle que busque solo en algunos modes. Encima el lio es total
+;; porque también existe la otra variable y no sé cómo interactúan...
+(setq cape-dabbrev-check-other-buffers nil
+      dabbrev-check-other-buffers nil)
+
+
+(provide 'ism-corfucape)
+;;; ism-corfucape.el ends here
