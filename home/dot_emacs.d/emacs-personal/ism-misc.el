814bd74355d1e082799f508e8c427253589ca1c2
Igor Sosa Mayor <joseleopoldo1792@gmail.com>
2025-09-01 22:16:34 +0200

    Many packages in misc for emacs.

--- home/dot_emacs.d/emacs-personal/ism-misc.el
+++ home/dot_emacs.d/emacs-personal/ism-misc.el
@@ -1,5 +1,8 @@
 (message "Loading other settings...")
 
+;; NOTE: here are a lot of configs which I test, I am not convinced of,
+;; I do not know where to put them...
+
 ; con esto se logra que no meta en el trash algunos ficheros temporales
 (setq system-trash-exclude-matches '("#[^/]+#$" ".*~$" "\\.emacs\\.desktop.*" "#.+"))
 (setq system-trash-exclude-paths '("/tmp"))
@@ -73,6 +76,26 @@
          ("/authorized_keys2?\\'" . ssh-authorized-keys-mode))
   :hook (((ssh-config-mode ssh-known-hosts-mode ssh-authorized-keys-mode) . turn-on-font-lock)))
 
+(use-package ace-window
+  :bind
+  (("C-x o" . ace-window)
+   ("H-o"   . ace-window))
+  :init (ace-window-display-mode 1)
+  :custom-face (aw-mode-line-face ((t (:inherit (bold mode-line-emphasis)))))
+  :config (setq aw-swap-invert t)
+  (setq aw-keys '(?q ?w ?e ?r ?t ?y ?u ?i ?p))
+  (setq aw-dispatch-alist
+        '((?k aw-delete-window "Delete Window")
+          (?x aw-swap-window "Swap Windows")
+          (?c aw-copy-window "Copy Window")
+          (?j aw-switch-buffer-in-window "Select Buffer")
+          (?o aw-flip-window)
+          (?b aw-switch-buffer-other-window "Switch Buffer Other Window")
+          (?c aw-split-window-fair "Split Fair Window")
+          (?s aw-split-window-vert "Split Vert Window")
+          (?v aw-split-window-horz "Split Horz Window")
+          (?o delete-other-windows "Delete Other Windows")
+          (?? aw-show-dispatch-help))))
 
 ;; Other packages
 (add-to-list 'load-path "~/giteando/emacs-request/")
@@ -89,6 +112,130 @@
 (keychain-refresh-environment)
 (defalias 'cargar-keychain 'keychain-refresh-environment)
 
+; lo de recentfiles
+(require 'recentf)
+(setq recentf-max-saved-items 100
+      recentf-exclude '("/tmp/" "/ssh:"))
+(recentf-mode +1)
+
+; Para que marque las regiones
+(setq transient-mark-mode t)
+
+; con esto se consigue que los buffer de ayuda etc se
+; abran abajo y no al lado...
+(setq split-height-threshold 0)
+(setq split-width-threshold nil)
+
+(eval-after-load 'gnus
+  '(load-library "gnus-toggle-topics")
+  )
+
+;; also tabs are evil
+(setq-default indent-tabs-mode nil)
+
+;; necesitamos esto porque si no no encuentra el typescript-language-server
+(setq exec-path (append exec-path '("~/.npm-global/bin")))
+(setq exec-path (append exec-path '("~/.local/bin")))
+(setq exec-path (append exec-path '("~/perl5/bin/" "~/perl5/lib/")))
+
+(when (executable-find "rg")
+  (setq grep-program "rg"))
+
+(when (executable-find "fd")
+  (setq find-program "fd"))
+
+;; me molesta que "n" abra el buffer donde está el "error" con rg...
+;; intercambiamos las teclas 
+(require 'rg)
+(define-key rg-mode-map (kbd "n") 'compilation-next-error)
+(define-key rg-mode-map (kbd "p") 'compilation-previous-error)
+(define-key rg-mode-map (kbd "M-n") 'next-error-no-select)
+(define-key rg-mode-map (kbd "M-p") 'previous-error-no-select)
+
+;; hace que no salga una window al abrir por ejemplo un vídeo con !
+;; en dired
+;; tomado de aquí:
+;; https://emacs.stackexchange.com/questions/5553/async-shell-process-buffer-always-clobbers-window-arrangement
+(add-to-list 'display-buffer-alist
+             (cons "\\*Async Shell Command\\*.*" (cons #'display-buffer-no-window nil)))
+
+(setq recenter-positions '(0.50 0.07 0.93)) ;default: '(middle top bottom)
+;; First C-l  -> 0.50: Put point vertically at the middle of the window
+;; Second C-l -> 0.07: Put point close to the top of the window. If
+;;                     (window-height) returns 70, that's roughly 4 lines.
+;; Third C-l  -> 0.93: Put point close to the bottom of the window ~ 3 lines.
+;; With the default values of `recenter-positions' and `scroll-margin' (0),
+;; the "top" position is the first line of the window, and the "bottom"
+;; position is the last line. Above settings provides a margin of 3 or 4 lines
+;; for my default window size for the "top" and "bottom" iterations.
+
+(setq mastodon-instance-url "https://fedihum.org"
+      mastodon-active-user "rogorido")
+
+(use-package mastodon
+  :ensure t
+  :config
+  (mastodon-discover))
+
+(add-hook 'prog-mode-hook #'hl-line-mode)
+(set-face-background hl-line-face "gray23")
+
+;; con esto conseguimos emojis!
+;; NOTE: atención esto es un lío del carajo... no sé si está cargando
+;; esto de alguna forma porque si pongo el curso sobre esto: 🏵️ y hago
+;; describe-char me sale que usa Noto pero si le digo que me describa la
+;; fuente no... Pero si busco en nerd-fonts me dice que ese signo no
+;; existe... Es decir, que lo está tomando de la otra...
+(setf use-default-font-for-symbols nil)
+(set-fontset-font t 'unicode "Noto Color Emoji" nil 'append)
+
+(all-the-icons-completion-mode)
+
+(use-package all-the-icons-ibuffer
+  :after all-the-icons
+  :ensure t
+  :hook (ibuffer-mode . all-the-icons-ibuffer-mode))
+
+(add-hook 'marginalia-mode-hook #'all-the-icons-completion-marginalia-setup)
+(add-hook 'dired-mode-hook #'all-the-icons-dired-mode)
+
+;; abre en mode view-mode todos los ficheros que no sean editables
+(setq view-read-only t)
+
+(add-hook 'package-menu-mode-hook 'hl-line-mode)
+(add-hook 'markdown-mode-hook  'turn-on-flyspell)
+
+(use-package kind-icon
+  :ensure t
+  :after corfu
+  ;:custom
+  ; (kind-icon-blend-background t)
+  ; (kind-icon-default-face 'corfu-default) ; only needed with blend-background
+  :config
+  (add-to-list 'corfu-margin-formatters #'kind-icon-margin-formatter))
+
+
+;; esto es muy importante!
+;; https://github.com/emacs-lsp/lsp-mode/issues/4454
+;; si no, me abre los ficheros js con vue-semantic-server!
+;; Is this necessary?
+(setq lsp-volar-take-over-mode nil)
+
+;; https://gist.github.com/lateau/5260613
+(defun dont-kill-emacs()
+  "Disable C-x C-c binding execute kill-emacs."
+  (interactive)
+  (error (substitute-command-keys "To exit emacs: \\[kill-emacs]")))
+(global-set-key (kbd "C-x C-c") 'dont-kill-emacs)
+;; esto también es posible pero pongo lo otro para que me advierta de cómo cerrar!
+;;(global-unset-key (kbd "C-x C-c"))
+
+;; gnutls.c: [1] Note that the security level of the Diffie-Hellman key exchange has been lowered to 256 bits and this may allow decryption of the session data
+;; https://bbs.archlinux.org/viewtopic.php?id72158
+;; TODO: delete?
+;;(setq gnutls-min-prime-bits 4096)
+
+(which-key-mode 1)
 
 (provide 'ism-misc)
 ;;; ism-misc.el ends here
