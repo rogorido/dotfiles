53c76f9cafac98a1100f5b81bb0ce3988d62f642
Igor Sosa Mayor <joseleopoldo1792@gmail.com>
2025-12-14 14:19:50 +0100

    New filters for EMMS.

--- home/dot_emacs.d/emacs-personal/ism-emms-ext.el
+++ home/dot_emacs.d/emacs-personal/ism-emms-ext.el
@@ -1,4 +1,4 @@
-(message "Loading emms-ext...")
+(message "Loading EMMS filters...")
 
 (setq ism/emms-general-filters
       '(
@@ -8,10 +8,12 @@
         ("Directory" "extraeuropea" "musica/extraeuropea")
         ("Directory" "tradicional" "musica/tradicional")
         ("Directory" "cantautores" "musica/cantautores")
+        ("Directory" "opera" "musica/opera")
+        ("Directory" "pop" "musica/pop")
 
         ("Genre" "Barroco"    "Barroco")
         ("Genre" "Early music"    "Early Music")
-        ("Genre" "Opera"    "opera")
+        ;; ("Genre" "Opera"    "opera")
 
         ("Artist" "mozart"  "Mozart")
         ("Artist" "Francesco Geminiani" "Geminiani")
@@ -20,8 +22,22 @@
         ("Artist" "Johann Rosenmüller" "Johann Rosenmüller")
         ("Artist" "John Dowland 1563-1626" "John Dowland")
         ("Artist" "Luigi Boccherini" "Luigi Boccherini")
+        ("Artist" "Luys Milan (c.1500-1561)" "Luys Milan")
         ("Artist" "Mateo Flecha, el Viejo" "Mateo Flecha, el Viejo")
         ("Artist" "Roland de Lassus (1536-1593)" "Roland de Lassus")
+        ("Artist" "Fernando Sor (1778-1839)" "Fernando Sor")
+        ("Artist" "Pierre Guédron (1570-1620)" "Pierre Guédron")
+        ("Artist" "Étienne Moulinié (1599-1676)" "Étienne Moulinié")
+        ("Artist" "Antoine Boësset (1587-1643)" "Antoine Boesset")
+        ("Artist" "Michel Lambert (1610-1696)" "Michel Lambert")
+
+        ;; crussell
+        ("Album" "Cuarteto de clarinete" "Clarinet Quartets") 
+        
+        ;; all "modern" music...
+        ("Multi-filter"
+         "trad++"
+         (("pop" "tradicional" "cantautores"))) 
 
         ("Multi-filter"
          "Barroco | Early Music"
@@ -32,6 +48,17 @@
          (("Opera") ("mozart")))
 
         ("Multi-filter"
+         "clásica española"
+         (("Fernando Sor (1778-1839)" "Mateo Flecha, el Viejo"
+           "Francisco Tárrega" "Luys Milan (c.1500-1561)")))
+
+        ;; air de cour
+        ("Multi-filter"
+         "Air de cour"
+         (("Étienne Moulinié (1599-1676)" "Pierre Guédron (1570-1620)" "Antoine Boësset (1587-1643)"
+           "Michel Lambert (1610-1696)")))
+
+        ("Multi-filter"
          "mozart (sin ópera)"
          (("mozart") (:not "Opera"))) ;; funciona pero mal por tags!
 
@@ -40,3 +67,4 @@
          (("clasica" "renacimiento")))))
 
 (provide 'ism-emms-ext)
+;;; ism-emms-ext.el ends here
